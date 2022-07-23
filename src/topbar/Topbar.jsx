b8e674a082471ccc7512b7e436fca817d0c30575
Marine <marine.lenoir@hotmail.fr>
2022-07-23 16:37:58 +0200

    FEAT: 1er commit Itération 1

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Topbar.css"
+
+export default function Topbar() {
+  return (
+    <div className='top'>
+        <div className='topLeft'>Left</div>
+        <div className='topCenter'>Center</div>
+        <div className="topRight">Right</div>
+    </div>
+  )
+}
