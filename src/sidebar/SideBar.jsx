a530787d32bb5bbaf19ee860eccb7c0f3cd79fd9
Marine <marine.lenoir@hotmail.fr>
2022-08-12 15:40:22 +0200

    FEAT: Itération 4: bloc About finished

--- src/sidebar/SideBar.jsx
+++ src/sidebar/SideBar.jsx
@@ -1,10 +1,13 @@
 import "./SideBar.css"
+import portraitabout from "./portraitabout.jpg"
 
 export default function SideBar() {
   return (
     <div className="sidebar">
         <div className="sidebarItem">
-            <span className="sidebarTitle">About</span>
+            <span className="sidebarTitle">ABOUT ME</span>
+            <img src={portraitabout} className="photoabout" alt="portait blogueuse"></img>
+            <p className="sideText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque.</p>
         </div>
     </div>
   )
--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -26,3 +26,4 @@ export default function Topbar() {
     </div>
   )
 }
+
