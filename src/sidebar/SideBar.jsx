a42b43d2ef352c282da35725cb9f888889457ecd
Marine <marine.lenoir@hotmail.fr>
2022-09-02 14:50:49 +0200

    Feat 2: Iteration 4 - finish Categories Bloc

--- src/sidebar/SideBar.jsx
+++ src/sidebar/SideBar.jsx
@@ -4,10 +4,21 @@ import portraitabout from "./portraitabout.jpg"
 export default function SideBar() {
   return (
     <div className="sidebar">
+      {/* BLOC ABOUT */}
         <div className="sidebarItem">
             <span className="sidebarTitle">ABOUT ME</span>
             <img src={portraitabout} className="photoabout" alt="portait blogueuse"></img>
             <p className="sideText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque.</p>
+        </div>
+      {/* BLOC CATEGORIES */}
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+            <ul className='sideList'>
+              <li className='sideListItems'>Destinations</li>
+              <li className='sideListItems'>Inspirations</li>
+              <li className='sideListItems'>Sweat Hotels</li>
+              <li className='sideListItems'>Foodtrip</li>
+            </ul>
         </div>
     </div>
   )
